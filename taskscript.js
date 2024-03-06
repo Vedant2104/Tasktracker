6ce8f184490bec5f9b63f369f8d5466392c09893
Vedant2104 <152850332+Vedant2104@users.noreply.github.com>
2024-03-06 11:05:27 +0530

    Add files via upload

---
+++ taskscript.js
@@ -0,0 +1,14 @@
+let taskadder = document.getElementById("addtask");
+let taskinput = document.getElementById("listtask");
+let box = document.getElementById("box");
+
+function createtask(){
+    //let newtask=document.createElement("input");
+    newtask.setAttribute("class","task");
+
+    box.insertBefore(newtask,taskadder);
+}
+
+taskadder.addEventListener("click",()=>{
+    createtask();
+})
